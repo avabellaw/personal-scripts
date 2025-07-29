bfb8e626953669484ebde5927937068e8b98a249
Ava Bella Williams <avabellawilliams@gmail.com>
2025-07-29 18:35:49 +0100

    Remove notfiy user on failure as doesn't work as root
    I have the Executor script that informs me anyway

---
+++ run-btrbk.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Runs btrbk and logs success/failure timestamp.
+
+LOG_FILE=/var/log/btrbk.log
+
+sudo btrbk run
+
+if [[ $? -eq 0 ]]
+then
+	echo -n "SUCCESS" >> $LOG_FILE # Log success
+else
+	echo -n "FAILURE" >> $LOG_FILE # Log failure
+fi
+
+echo ": " $(date) >> $LOG_FILE # Log timestamp
