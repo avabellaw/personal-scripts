aa7e0581937f95bd69f0164b28b8a809d7b97205
Ava Bella Williams <avabellawilliams@gmail.com>
2025-08-29 01:52:14 +0100

    run-btrbk: Add incremental backup of /boot

--- run-btrbk.sh
+++ run-btrbk.sh
@@ -2,21 +2,39 @@
 
 # Runs btrbk and logs success/failure timestamp.
 
+# REQUIRES:
+# - btrfsroot to be mounted. This is done in btrbk.service.
+# rsync-backup to be on program path.
+
 LOG_FILE=/var/log/btrbk.log
 
 TIME_START=$(date "+%s")
 
+# Run btrbk
 sudo btrbk run
 
 TIME_END=$(date "+%s")
-
 TIME_TAKEN=$((TIME_END-TIME_START))
 
 if [[ $? -eq 0 ]]
 then
 	echo -n "SUCCESS" >> $LOG_FILE # Log success
 else
-	echo -n "FAILURE" >> $LOG_FILE # Log failure
+	echo "FAILURE" >> $LOG_FILE # Log failure
+    exit $?
 fi
 
-echo ": " $(date) " Completed in: ${TIME_TAKEN}s">> $LOG_FILE # Log timestamp
+# Log the date and time taken to complete backup.
+echo -n ": " $(date) " Completed in: ${TIME_TAKEN}s">> $LOG_FILE # Log timestamp
+
+# Run rsync on /boot directory.
+# Backing up to root means that the correct /boot version will be tied to each root snapshot.
+backedup=$(/home/ava/Scripts/bin/rsync-backup /boot /boot-backup)
+
+#If not empty
+if [ -n "$(echo "$backedup" | tr -d '[:space:]')" ];
+then
+    echo " /boot changed - BACKUP: $backedup" >> $LOG_FILE;
+else
+    echo "" >> $LOG_FILE;
+fi
