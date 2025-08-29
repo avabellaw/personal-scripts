a5c3870bca0128fd10699fd4881bb2f305e123ed
Ava Bella Williams <avabellawilliams@gmail.com>
2025-08-29 11:07:38 +0100

    Move /boot backup to before snapshot and add /boot error handling

--- run-btrbk.sh
+++ run-btrbk.sh
@@ -10,6 +10,21 @@ LOG_FILE=/var/log/btrbk.log
 
 TIME_START=$(date "+%s")
 
+# Run rsync on /boot directory, before snapshots.
+# Backing up to root means that the correct /boot version will be tied to each root snapshot.
+BOOT_BACKUP_DATETIME=$(/home/ava/Scripts/bin/rsync-backup /boot /boot-backup)
+
+#If not empty
+BOOT_CHANGED=$( [ -n "$(echo "$BOOT_BACKUP_DATETIME" | tr -d '[:space:]')" ] && echo true || echo false)
+
+echo "BOOTH CHANGED: " $BOOT_CHANGED
+
+if [ $? -ne 0 ];
+then
+    echo "BOOT BACKUP FAILURE";
+    BOOT_CHANGED="-1"
+fi
+
 # Run btrbk
 sudo btrbk run
 
@@ -19,6 +34,7 @@ TIME_TAKEN=$((TIME_END-TIME_START))
 if [[ $? -eq 0 ]]
 then
 	echo -n "SUCCESS" >> $LOG_FILE # Log success
+
 else
 	echo "FAILURE" >> $LOG_FILE # Log failure
     exit $?
@@ -27,14 +43,12 @@ fi
 # Log the date and time taken to complete backup.
 echo -n ": " $(date) " Completed in: ${TIME_TAKEN}s">> $LOG_FILE # Log timestamp
 
-# Run rsync on /boot directory.
-# Backing up to root means that the correct /boot version will be tied to each root snapshot.
-backedup=$(/home/ava/Scripts/bin/rsync-backup /boot /boot-backup)
-
-#If not empty
-if [ -n "$(echo "$backedup" | tr -d '[:space:]')" ];
+if [ $BOOT_CHANGED = "-1" ];
 then
-    echo " /boot changed - BACKUP: $backedup" >> $LOG_FILE;
+    echo " /boot changed - BACKUP FAILED!" >> $LOG_FILE;
+elif [ $BOOT_CHANGED = true ];
+then
+    echo " /boot changed - BACKUP: $BOOT_BACKUP_DATETIME" >> $LOG_FILE;
 else
     echo "" >> $LOG_FILE;
 fi
