5aa698ca73e654d65ffce08308ba9299047050c2
avabellaw <avabellawilliams@gmail.com>
2025-12-13 12:24:55 +0000

    run-btrbk.sh: Make boot backup dir if doesnt exist

--- run-btrbk.sh
+++ run-btrbk.sh
@@ -3,16 +3,20 @@
 # Runs btrbk and logs success/failure timestamp.
 
 # REQUIRES:
-# - btrfsroot to be mounted. This is done in btrbk.service.
+# - btrfsroot to be mounted. This is done in btrbk.service using mnt-btrbk.mount.
 # rsync-backup to be on program path.
 
 LOG_FILE=/var/log/btrbk.log
 
 TIME_START=$(date "+%s")
 
+BOOT_BACKUP_DIR=/boot-backup
+
+mkdir -p $BOOT_BACKUP_DIR
+
 # Run rsync on /boot directory, before snapshots.
 # Backing up to root means that the correct /boot version will be tied to each root snapshot.
-BOOT_BACKUP_DATETIME=$(/home/ava/Scripts/bin/rsync-backup /boot /boot-backup)
+BOOT_BACKUP_DATETIME=$(/home/ava/Data/Scripts/bin/rsync-backup /boot $BOOT_BACKUP_DIR)
 
 #If not empty
 BOOT_CHANGED=$( [ -n "$(echo "$BOOT_BACKUP_DATETIME" | tr -d '[:space:]')" ] && echo true || echo false)
